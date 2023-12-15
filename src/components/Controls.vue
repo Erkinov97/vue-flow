e716fb9cf31ee9527bac77be88013b6a9cbf8d1f
erkinov97 <erkinov997@gmail.com>
2023-12-15 05:28:06 +0500

    fix text

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -52,7 +52,7 @@ function onAdd() {
   <Panel position="top-right" class="save-restore-controls">
     <button class="text-white bg-blue-500 border p-2" @click="onSave">Saqlash</button>
     <button class="text-white bg-blue-500 border p-2" @click="onRestore">
-      O'chirish
+      Saqlanmagan amallarni bekor qilish 
     </button>
     <button class="text-white bg-blue-500 border p-2" @click="onAdd">Yangi element qo'shish</button>
   </Panel>
